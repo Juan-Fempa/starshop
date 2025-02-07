599cbd37f59cfd40d5165351750f41c3aa2a75b3
Juan CASA <belda_juamol@gva.es>
2025-02-07 20:58:30 +0100

    ADD ruta padre

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -7,13 +7,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController
 {
-    #[Route('/api/starships')]
+    #[Route('')]
     public function getCollection(StarshipRepository $repository): Response
     {
         $starships = $repository->findAll();
 
         return $this->json($starships);
+    }
+
+    #[Route('/{id<\d+>}')]
+    public function get(StarshipRepository $repository, int $id)
+    {
+        $starships = $repository->findAll();
+
+        return $this->json($starships[$id]);
+
     }
 }
